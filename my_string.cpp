0e663aff08dc505e66f913c6895e69e42f279c32
TerryZjl <1538068051@qq.com>
2017-03-19 18:52:49 +0800

    my string

--- my_string.cpp
+++ my_string.cpp
@@ -1,3 +1,214 @@
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//#pragma warning (disable:4996)
+//
+//class String
+//{
+//    friend ostream& operator<<(ostream& out, const String& s);
+//public:
+//	String(char *str = "")                       //默认参数是一个空字符串，是一个随机地址
+//		:_str(new char[(strlen(str)+1)])         //为str开辟一段空间，进行初始化
+//	{
+//
+//		strcpy(_str, str);
+//		cout << "String(const char* s)" << endl;
+//	}
+//
+//	////传统深拷贝
+//	//String(const String& s)
+//	//	:_str(new char[strlen(s._str) + 1])    //深拷贝构造函数
+//	//{
+//	//	strcpy(_str, s._str);
+//	//	cout << "String(const String& s)" << endl;
+//	//}
+//
+//	//交换的方式拷贝构造
+//	String(const String& s)     
+//		:_str(NULL)              //若是 String str3; str3 = str1; 这种情况，交换赋值时需要调用拷贝构造，
+//	{                            //str3._str是创建好的一个空字符串，地址是一个随机的地址，不为NULL，
+//		String tmp(s._str);      // 交换内容后，下面析构tmp时就会调用delete，但是并没有动态开辟空间；所以需要在这里把_str置空
+//		std::swap(_str, tmp._str);
+//		cout<<"String(const String& s)"<<endl;
+//	}
+//	////传统赋值
+//	//String& operator=(const String& s2)                   //想要str1 = str2，不能直接让str1指向str2那段空间，必须从新开辟一段，否则会在析构时出问题
+//	//{
+//	//	delete[] _str;                                     //先把this指针指向的_str的空间释放了,再给_str开辟新的空间
+//	//	if (_str != s2._str)
+//	//	{
+//	//		_str = new char[strlen(s2._str) + 1];
+//	//		strcpy(_str, s2._str);
+//	//	}
+//	//	return *this;
+//	//}
+//	//交换赋值方法
+//	String& operator=(String s2)     //这里调用拷贝构造函数开辟一段空间复制s2，然后交换s2和*this的字符串
+//	{
+//		std::swap(_str, s2._str);
+//		return *this;
+//	}
+//
+//	~String()
+//	{
+//		cout << "~String() " << endl;
+//		if (_str != NULL)
+//		{ 
+//			delete[] _str;
+//			_str = NULL;
+//		}
+//	}
+//
+//	//void CheckCapacity(int count)
+//	//{
+//	//	if (_size + count >= _capacity)
+//	//	{
+//	//		int _count = (2 * _capacity)>(_capacity + count) ? (2 * _capacity) : (_capacity + count);
+//	//		char *tmp = new char[_count];
+//	//		strcpy(tmp, _pstr);
+//	//		delete[]_pstr;
+//	//		_pstr = tmp;
+//	//		_capacity = _count;
+//	//	}
+//	//}
+//private:
+//	char *_str;
+//
+//};
+//
+//
+//ostream& operator<<(ostream& out, const String& s)
+//{
+//	out << s._str;
+//	return out;
+//}
+//
+//
+//int main()
+//{  
+//	String str1 = "pppppppp";
+//	String str2(str1);
+//	String str3;
+//	str3 = str1;
+//	cout << str1 << endl;
+//	cout << str2 << endl;
+//
+//	return 0;
+//}
+
+
+
+
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//#pragma warning (disable:4996)
+//
+//class String
+//{
+//	friend ostream& operator<<(ostream& out, const String& s);
+//public:
+//	String(char *str = "")                       //默认参数是一个空字符串，是一个随机地址
+//		:_str(new char[(strlen(str) + 1)])       //为str开辟一段空间，进行初始化
+//		, _size(new int(1))
+//	{
+//		strcpy(_str, str);
+//		cout << "String(const char* s)" << endl;
+//	}
+//
+//	////传统深拷贝
+//	//String(const String& s)
+//	//	:_str(new char[strlen(s._str) + 1])    //深拷贝构造函数
+//	//{
+//	//	strcpy(_str, s._str);
+//	//	cout << "String(const String& s)" << endl;
+//	//}
+//
+//	//写时拷贝
+//	String(const String& s)
+//		:_str(s._str)
+//		, _size(s._size)
+//	{
+//		++(*_size);
+//	}
+//
+//	//写时赋值s1 = s2
+//	//String& operator=(const String& s)
+//	//{
+//	//	if (_str != s._str)
+//	//	{
+//	//		CheckRelease();      
+//	//		_str = s._str;     //让s1._str指向s2._str
+//	//		_size = s._size;   //让s1._size指向s2._size
+//	//		++(*s._size);      //引用计数加一
+//
+//	//	}
+//	//}
+//	String& operator=(String s)
+//	{
+//		swap(_str, s._str);
+//		swap(_size, s._size);    //s在函数调用完就自动释放了
+//		return *this;
+//
+//	}
+//	////传统赋值
+//	//String& operator=(const String& s2)                   //想要str1 = str2，不能直接让str1指向str2那段空间，必须从新开辟一段，否则会在析构时出问题
+//	//{
+//	//	delete[] _str;                                     //先把this指针指向的_str的空间释放了,再给_str开辟新的空间
+//	//	if (_str != s2._str)
+//	//	{
+//	//		_str = new char[strlen(s2._str) + 1];
+//	//		strcpy(_str, s2._str);
+//	//	}
+//	//	return *this;
+//	//}
+//
+//
+//	void CheckRelease()   //检查要被改变的s1所指向的空间是否只有s1一个对象指着，若是则释放
+//	{
+//		if (--(*_size) == 0)
+//		{
+//			delete[] _str;
+//			_str = NULL;
+//		}
+//	}
+//
+//	~String()
+//	{
+//		CheckRelease();
+//		cout << "~String() " << endl;
+//	}
+//
+//
+//private:
+//	char *_str;
+//	int *_size;
+//};
+//
+//
+//ostream& operator<<(ostream& out, const String& s)
+//{
+//	out << s._str;
+//	return out;
+//}
+//
+//
+//int main()
+//{
+//	String str1 = "pppppppp";
+//	String str2(str1);
+//	String str3;
+//	/*str3 = str1;*/
+//	cout << str1 << endl;
+//	cout << str2 << endl;
+//
+//	return 0;
+//}
+//
+
+
 
 
 #include<iostream>
@@ -29,7 +240,7 @@ public:
 		size_t len = _size + count;
 		if (len > _capacity)
 		{
-			char* tmp = (char*)realloc(_str, len);
+			char* tmp = (char*)realloc(_str, len+1);  //开辟空间时len+1是总长，不+1就没地放\0
 			if (tmp != NULL)
 			{
 				_str = tmp;
@@ -77,6 +288,29 @@ public:
 		_str[pos] = ch;
 		++_size;
 	}
+
+	String SubStr(size_t pos, size_t len)
+	{
+		assert(pos < _size);
+		size_t count = len;
+		if (pos + len>_size - 1)
+		{
+			count = _size - pos;
+		}
+
+		String Sub;
+		Sub.CheckCapacity(count);
+		char* dst = Sub._str;
+		char* src = _str + pos;
+		for (size_t i = 0; i < count;i++)
+		{
+			dst[i] = src[i];
+		}
+		dst[count] = '\0';
+		Sub._size = count;
+		return Sub;
+	}
+
 	//插入指定字符串
 	void Insert(size_t pos, const char* str)
 	{
@@ -84,8 +318,8 @@ public:
 
 		int len = strlen(str);
 		CheckCapacity(_size + len);
-		size_t end = _size;
-		while (end >= pos)
+		int end = _size;
+		while (end >= (int)pos)//1 2 3 4 5 6
 		{
 			_str[end + len] = _str[end];
 			--end;
@@ -96,28 +330,24 @@ public:
 			_str[pos + i] = str[i];
 			--len;
 			++i;
+			_size++;
 		}
 	}
 	//删除指定长度字符串
 	void Erase(size_t pos, size_t len)
 	{
 		assert(pos < _size);
-		if ((pos + len)>_size)
+		if ((pos + len) >= _size)
 		{
-			_str[pos] = '\0';
-			++pos;
-			_size = strlen(_str);
+			_size = pos;
+			_str[_size] = '\0';
 		}
 		else
 		{
-			size_t i = len;
-			while (i>0)
-			{
-				_str[pos] = _str[pos+len];
-				--i;
-				++pos;
-			}
-			_size = strlen(_str);
+			char* dst = _str + pos + len;
+			char* src = _str + pos;
+			strcpy(src, dst);
+			_size -= len ;
 		}
 
 	}
@@ -131,6 +361,30 @@ public:
 			if (ch == _str[j])
 			{
 				return j;
+			}
+		}
+		return -1;
+	}
+	int Find(char* str)
+	{
+		int i = strlen(_str);
+		int j = 0;
+		for (j = 0; j < i; j++)
+		{
+			int n = j;
+			char* tmp = str;
+			while(*tmp == _str[n])
+			{
+				tmp++;
+				n++;
+			}
+			if (*tmp == '\0')
+			{
+				return j;
+			}
+			else
+			{
+				continue;
 			}
 		}
 		return -1;
@@ -182,22 +436,40 @@ ostream& operator<<(ostream& out, String& s)
 
 int main()
 {
-	String s1("abcdef");
+    String s1("abaaaaaacdef");
 	String s2("123456");
-	//s1[5] = 'a';     ////增
+	//s1[5] = 'a';     //增
 	//s1.PushBack('a');   ////尾插
-	//s1.Insert(4, s2, 3, 3);  //插一串
-	//s2.Erase(3, 2);        //删
-	int ret = s2.Find(2);
-	if (ret == -1)
-	{
-		cout << "没找到" << endl;
-	}
-	else
-	{
-		cout << "下标[ret]" << endl;
-	}
+	//s1.Insert(0, "c");  //插一串
+	//s2.Erase(0, 2);        //删
+	String tmp = s1.SubStr(2, 6);
+	
+	//int ret = s2.Find("345");
+	//if (ret == -1)
+	//{
+	//	cout << "没找到" << endl;
+	//}
+	//else
+	//{
+	//	cout << "在下标[" <<ret<<"]处"<< endl;
+	//}
 	cout << s1 << endl;
+	cout << tmp << endl;
 	cout << s2 << endl;
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
